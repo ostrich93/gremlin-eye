5265c5191e1493213bb520a57d75b02577cd39fd
ostrich93 <alexmorson95@gmail.com>
2025-08-30 16:36:16 -0500

    Fixed the Login page to better take into account unsuccessful logins

--- gremlin-eye.client/src/pages/Login.jsx
+++ gremlin-eye.client/src/pages/Login.jsx
@@ -3,28 +3,38 @@ import { useNavigate } from 'react-router-dom';
 import { Button, Container, Form, Row } from 'react-bootstrap';
 import { useAuthState, useAuthDispatch } from '../contexts/AuthProvider';
 import { login } from '../actions/authActions';
+import useDebounce from '../hooks/useDebounce';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
-    const { loading, error } = useAuthState();
+    const { loading, error, refreshToken } = useAuthState();
     const dispatch = useAuthDispatch();
 
-    const handleLogin = async (e) => {
+    const submitLogin = (e) => {
         e.preventDefault();
-        await login(dispatch, { username: username, password: password });
-        navigate("/");
+        login(dispatch, { username: username, password: password });
     };
+
+    const handleLogin = () => {
+        if (refreshToken != null && refreshToken.length > 0) {
+            navigate("/");
+        }
+        if (error != null) {
+            alert("Failed to login. A user with the credentials inputted could not be found.");
+        }
+    };
+
+    useDebounce(handleLogin, 200, [refreshToken, navigate, error]);
 
     return (
         <Container>
             <Row id="log-in" className="justify-content-md-center mt-3 mx-2">
                 <div className="col-md-5 mt-2 mx-2 p-3">
                     <h2 id="title" className="text-center">Log In</h2>
-                    {error ? (<><p>{error}</p></>) : null}
-                    <Form onSubmit={handleLogin}>
+                    <Form onSubmit={submitLogin}>
                         <Form.Group className="my-3">
                             <Form.Control type='text' value={username} placeholder='Username' onChange={e => setUsername(e.target.value)} name="username" required disabled={loading} />
                         </Form.Group>
